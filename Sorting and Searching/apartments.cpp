d963a8d675a932ccdede97204d0d14a04b8ed112
Lakshay Angrish <lakshay.angrish@hotmail.com>
2020-07-27 07:40:25 +0530

    +6 Sorting and Searching

---
+++ Sorting and Searching/apartments.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvl;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<pair<int, int> > vp;
+typedef pair<int, int> pr;
+typedef tuple<int, int, int> ti;
+
+#define FOR(i, a, b) for (int i = (a), _b = (b); i < _b; i++)
+#define FORD(i, b, a) for (int i = (b), _a = (a); i > _a; i--)
+#define pb push_back
+#define print(v) cout << v << '\n'
+
+#define pr_vec(v) do {					\
+	for (auto i: v)	cout << i << ' ';	\
+	cout << '\n';						\
+} while (false)
+
+#define pr_mat(mat) do {					\
+	for (auto i: mat) {						\
+		for (auto j: i)	cout << j << ' ';	\
+		cout << '\n';						\
+	}										\
+} while (false)
+
+#define vin(v)  for (auto &i: v)    cin >> i
+
+#define matin(mat) do {			\
+	for (auto &i: mat)	vin(i);	\
+} while (false)
+
+ld pi = 3.141592653589793238;
+ll M = 1e9 + 7;
+
+ll gcd(ll a, ll b) {
+    if (a == 0) return b;
+
+    return gcd(b % a, a);
+}
+
+bool is_prime(ll n) {
+    if (n == 2) return true;
+    if (n < 2) return false;
+    if (n % 2 == 0) return false;
+
+    for (int i = 3; i <= sqrt(n); i += 2) {
+        if (n % i == 0) return false;
+    }
+
+    return true;
+}
+
+bool is_palindrome(string s) {
+    return equal(s.rbegin(), s.rend(), s.begin());
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t, n, m, k, q, x, y;
+    string s;
+    bool flag;
+
+	cin >> n >> m >> k;
+	vll a(n);
+	vin(a);
+	vll b(m);
+	vin(b);
+
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
+
+	int i = 0, j = 0;
+	q = 0;
+	while (i < n && j < m) {
+		if (abs(a[i] - b[j]) <= k) {
+			q++;
+			i++;
+			j++;
+		} else if (a[i] > b[j])	j++;
+		else i++;
+	}
+
+	print(q);
+
+    return 0;
+}
